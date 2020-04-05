5ce4599a9ada6e530258f8a698b172ef60ef8621
bunleapkali_ <61858680+bunleapkali@users.noreply.github.com>
2020-04-05 11:03:08 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Bunleap-Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Bunleap Sorn`,
+      summary: `who is a student and an independent developer in Cambodia.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      github: `bunleaps_`,
     },
   },
   plugins: [
